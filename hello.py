6405a90d397042cfd7ba98f3284a616e3dd1441f
Sophia <sophiaraji@gmail.com>
2020-05-30 20:54:57 -0400

    Added user sessions and redirects (GET)

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -29,9 +29,9 @@ def index():
 	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
+		session[name] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
