b997a2c50da0d8c69fd9f5668261eb058aa24e25
Sophia <sophiaraji@gmail.com>
2020-05-30 20:03:08 -0400

    Add forms to index using Flask-WTF

--- hello.py
+++ hello.py
@@ -2,11 +2,19 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,9 +24,14 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
