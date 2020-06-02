34d1e7ec6a8014f4b5c8601d4ba6cc739f438348
Sophia <sophiaraji@gmail.com>
2020-06-01 20:28:18 -0400

    Added asynchronous email support

--- hello.py
+++ hello.py
@@ -27,12 +27,20 @@ app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <sophia@flasky.com>'
 
+# Asynchronous Email Support
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
+
 def send_email(to, subject, template, **kwargs):
 	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
 		sender = app.config['FLASKY_MAIL_SENDER'], recipients = [to])
 	msg.body = render_template(template + '.txt', **kwargs)
 	msg.html = render_template(template + '.html', **kwargs)
 	mail.send(msg)
+	thr = Thread(target=send_async_email, args = [app, msg])
+	thr.start()
+	return thr
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
